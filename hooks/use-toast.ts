d3e08f9b52e932322748eb438e57167b5d8938d3
mashanisimova <masha.anisi@gmail.com>
2025-05-29 06:33:55 +0200

    Delete hooks directory

--- hooks/use-toast.ts
+++
@@ -1,5 +0,0 @@
-// Import hooks from the shadcn/ui toast component
-import { useToast as useShadcnToast } from "@/components/ui/use-toast";
-
-// Re-export the hook with the same name
-export const useToast = useShadcnToast;
